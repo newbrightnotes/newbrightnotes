791af5c43271086770e6c4198bc2e90a8f84e2b2
Marcos Tamoyo Freire <marcostamoyofreire@gmail.com>
2025-10-27 16:57:03 -0300

    fix: remove data-nscript from AdSense script to eliminate console warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -177,11 +177,11 @@ export default function RootLayout({
         </Script>
 
         {/* Google AdSense */}
-        <Script
+        <script
+          async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8502616335999449"
-          strategy="afterInteractive"
           crossOrigin="anonymous"
-        />
+        ></script>
 
         {/* Structured Data - Organization */}
         <Script
